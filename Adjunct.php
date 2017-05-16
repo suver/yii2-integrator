8208eb3a7e07679f4bcb7d2a901b5d5f77defd7f
Denis B <suver@farpse.com>
2017-05-16 14:58:10 +0300

    Integrator repo

---
+++ Adjunct.php
@@ -0,0 +1,15 @@
+<?php
+namespace suver\logger;
+
+
+/**
+ * @author Suver <suver@inbox.ru>
+ * @link https://github.com/suver/yii2-logger
+ */
+class Adjunct
+{
+	public function export()
+	{
+
+	}
+}
